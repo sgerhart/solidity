83470770ae16adeab8cb9cfbf5332ad43f055dca
"Steven Derek Gerhart" <"sgerhart@gmail.com">
2022-04-29 14:44:17 -0400

    block.timestap

--- training/SendMoneyExample.sol
+++ training/SendMoneyExample.sol
@@ -5,22 +5,28 @@ pragma solidity ^0.8.1;
 contract SendMoneyExample {
 
     uint public balanceReceived;
+    uint public moneyBlocked;
 
     function receiveMoney() public payable {
         balanceReceived += msg.value;
+        moneyBlocked = block.timestamp + 1 minutes;
     }
 
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
 
-    //function getMoney() public {
-    //    address payable to = payable(msg.sender);
-   //     to.transfer(getBalance());
-    //}
+    function getMoney() public {
+        if(moneyBlocked < block.timestamp) {
+            address payable to = payable(msg.sender);
+            to.transfer(getBalance());
+        }
+    }
 
 
     function withdrawMoneyTo(address payable _to) public {
-        _to.transfer(getBalance());
+        if (moneyBlocked < block.timestamp) {
+           _to.transfer(getBalance());
+        }
     }
 }
